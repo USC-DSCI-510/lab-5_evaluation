b715218e3d2c8ecc15b7de06faa0ac5d7968b806
Kayvan Shah <kayvan.shah@usc.edu>
2023-09-29 03:26:17 -0700

    added exception cases

--- tests/test_private.py
+++ tests/test_private.py
@@ -211,3 +211,35 @@ def test_analyse_student_data(filename, ans):
 def test_analyse_bank_data(filename, ans):
     filename = os.path.join(DATA_DIR, "bank", filename)
     assert analyse_bank_data(filename) == ans
+
+
+@pytest.mark.parametrize(
+    "filename,ans",
+    [
+        ("student_performance_test_1_dne.csv", "Invalid Input"),
+        ("student_performance_test_2_dne.csv", "Invalid Input"),
+        ("student_performance_test_3_dne.txt", "Invalid Input"),
+    ],
+)
+@pytest.mark.timeout(0.25)
+def test_analyse_bank_data_does_not_exist(filename, ans):
+    filename = os.path.join(DATA_DIR, "bank", filename)
+    with pytest.raises(Exception) as e_info:
+        analyse_bank_data(filename)
+    assert str(e_info.value).lower() == ans.lower()
+
+
+@pytest.mark.parametrize(
+    "filename,ans",
+    [
+        ("bank_transactions_sample_dne.csv", "Invalid Input"),
+        ("bank_transactions_test_dne_234.csv", "Invalid Input"),
+        ("bank_transactions_dne_767.txt", "Invalid Input"),
+    ],
+)
+@pytest.mark.timeout(0.25)
+def test_analyse_student_data_does_not_exist(filename, ans):
+    filename = os.path.join(DATA_DIR, "student", filename)
+    with pytest.raises(Exception) as e_info:
+        analyse_bank_data(filename)
+    assert str(e_info.value).lower() == ans.lower()
